b926dd602d13375e419d82d5d1c6c082c4b764ee
Brit Smith <Bsmith@skillstorm.com>
2021-10-13 09:09:19 -0500

    fetch implemented for rendering items

--- public/javascript-files/main.js
+++ public/javascript-files/main.js
@@ -1,29 +1,55 @@
 
 
+alert('hey');
+
+const itemsList = document.querySelector('.items-list');
 
 
+const url = 'http://localhost:8085/items';
+
+let output =``;
 
 
-
-
-
-const getItems = async () =>{
-    const response = await fetch('')
-    const items = await response.json();
-
-    return items;
-
+const renderItems =(items) =>{
+    items.forEach( item =>{
+        output += `
+         <div class="item-display mt-4 col-md-6 bg-dark">
+             <div class="item-body">
+                 <div class="stuff">
+                     <h1 class="itemName"> ${item.itemName} </h4>
+                     <h3 class="amount"> ${item.amount} </h2>
+                     <h3 class="price"> ${item.price} </h3>
+                     <p class="description"> ${item.description} </p>
+                     <a href="#" class= "item-link">Edit</a>
+                     <a href="#" class="item-link">Delete</a>
+                 </div>
+             </div>
+         </div>
+        `;
+      });
+      itemsList.innerHTML = output;
 }
 
+// Get - load all items
+// Method : GET
 
+ fetch(url)
+ .then(res => res.json())
+ .then(data => renderItems(data));
+
+    //return items;
+
+
+
+/*
 document.addEventListener("DOMContentLoaded", async () =>{
     let items =[];
     try {
-     items = await getItems();
+     items = await getAllItems();
     } catch(err){
         console.log("Error!");
         console.log(error);
     }
 
     console.log(items);
-})
+})*/
